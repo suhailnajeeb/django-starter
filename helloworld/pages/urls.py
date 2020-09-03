0c94ef14d90c0c2bd7adfe8a4776c07ae0fd7ea7
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-03 11:16:07 +0600

    bugfix for django 3.1

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
+from .views import homePageView         # Update for django 3.1 - You need to use .views insted of from . import views
 
-from . import views
-
-ulrpatterns = [
-    path('', views.homePageView, name = 'home')
+urlpatterns = [
+    path('', homePageView, name='home'),
 ]
