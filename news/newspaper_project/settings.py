bb2b2ad004b58c2dc83783f70ccbe4ef99a7bfb6
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-08 23:45:37 +0600

    apply bootstrap templates

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'crispy_forms',
+    # Local
     'users',
     'pages',
 ]
@@ -126,3 +129,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
