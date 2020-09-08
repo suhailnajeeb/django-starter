61b677a0493c8c369cf8f58ae79a11d8525df6ec
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-08 12:06:15 +0600

    typo fix

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import SignUpView
 
 urlpatterns = [
-    path('signup/', SingUpView.as_view(), name = 'signup'),
+    path('signup/', SignUpView.as_view(), name = 'signup'),
 ]
