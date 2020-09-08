c28c4edca40fd4d8a77331f644dea39304238def
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-08 22:52:08 +0600

    user authentication

--- news/users/forms.py
+++ news/users/forms.py
@@ -6,11 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields
+        fields = ('username', 'email', )
     
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
-
+        fields = ('username', 'email', )
