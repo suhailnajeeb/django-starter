796e936024d0b761c096dbb965f81b9cccfc364a
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-11 00:12:59 +0600

    update views for article

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -8,3 +9,17 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+class ArticleDetailView(ListView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(ListView):
+    model = Article
+    fields = ['title', 'body', ]
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(ListView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
