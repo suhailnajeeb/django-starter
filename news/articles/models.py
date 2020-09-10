37b1b59c1b3c02cceee0c48f8b18b2768fe34988
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-10 23:55:33 +0600

    udpate class name

--- news/articles/models.py
+++ news/articles/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Articles(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length = 255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
