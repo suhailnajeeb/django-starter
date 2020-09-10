91a8c486cc406009215fdbc608050c8bb15104d4
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-09-11 00:28:04 +0600

    update urls

--- news/articles/urls.py
+++ news/articles/urls.py
@@ -4,7 +4,7 @@ from .views import ArticleListView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name = 'article_list'),
-    path('<int:pk>/edit/', ArticleListView.as_view(), name = 'article_list'),
+    path('<int:pk>/edit/', ArticleListView.as_view(), name = 'article_edit'),
     path('<int:pk>/', ArticleListView.as_view(), name = 'article_detail'),
     path('<int:pk>/delete/', ArticleListView.as_view(), name = 'article_delete'),
 ]
